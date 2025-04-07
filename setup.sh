0e45297d4e26e0311cd6025b790b04f2cb83e34f
kreciorek <150957687+kreciorek@users.noreply.github.com>
2025-04-07 20:16:29 +0200

    treewide: add build and setup script
    much thanks for figuring out the build script so it works, codeinvers s/o
    note: script updated for latest mesa 25 devel branch
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+meson setup build/ --prefix=/usr --libexecdir=lib --sbindir=bin --auto-features=enabled \
+   -Dbuildtype=release \
+   -Dplatforms=x11,wayland \
+   -Dgallium-drivers=r600,radeonsi \
+   -Dvulkan-drivers=amd \
+   -Degl=enabled \
+   -Dgbm=enabled \
+   -Dglx=dri \
+   -Dllvm=enabled \
+   -Dshared-llvm=enabled \
+   -Ddraw-use-llvm=true \
+   -Damd-use-llvm=true \
+   -Dshader-cache=enabled \
+   -Dgallium-va=enabled \
+   -Dgallium-mediafoundation=disabled \
+   -Dvalgrind=disabled \
+   -Dlibunwind=disabled \
+   -Dlmsensors=disabled \
+   -Dzstd=enabled \
+   -Dgles1=enabled \
+   -Dgles2=enabled \
+   -Dxmlconfig=enabled \
+   -Dandroid-libbacktrace=disabled
